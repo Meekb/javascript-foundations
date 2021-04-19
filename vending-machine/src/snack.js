7e147b230092e9727f9a41a6a0af1acd72f9f2b5
Beth <bethm.meeker@gmail.com>
2021-04-19 08:02:24 -0600

    Add partial vending machine

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,22 @@
 class Snack {
+  constructor(item, cost) {
+    this.name = item;
+    this.price = cost;
+    this.itemsInStock = 0;
+  }
 
-}
+  stockItems(num) {
+    this.itemsInStock += num;
+  }
+
+  removeItem() {
+    if (!this.itemsInStock) {
+      return `Sorry, no ${this.name} left in stock!`
+    }
+    this.itemsInStock -= 1;
+    return  `Item taken! There are now ${this.itemsInStock} items left.`
+  }
+
+};
 
 module.exports = Snack;
--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,21 @@
 class VendingMachine {
+  constructor(vendingObj) {
+    this.id = vendingObj.id;
+    this.isBroken = vendingObj.isBroken;
+    this.snacks = [];
+  }
 
-}
+  addSnacks(item) {
+    if (this.snacks.includes(item.name)) {
+      return 'Sorry, that snack is already stocked! Try adding a different snack.'
+    } else {
+      this.snacks.push(item);
+      item.itemsInStock++
+    }
+  }
+
+
+
+};
 
 module.exports = VendingMachine;
